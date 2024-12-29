f6cdbc66e660c99499c2a48447ebb8a90dfbd864
PThinh440 <npthinh212@clc.fitus.edu.vn>
2024-12-29 16:41:45 +0700

    apply SOM

--- ClientDatabase/ClientSQL.cs
+++ ClientDatabase/ClientSQL.cs
@@ -1,24 +1,50 @@
 ﻿using SEP.Interfaces;
-using System;
-using SEP.Ultils;
-using SEP.CustomClassBuilder;
-using System.Xml.Linq;
-using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
 
 namespace SEP.ClientDatabase
 {
     internal class ClientSQL
     {
         private string connectionString;
+        private Server server;
         private SqlConnection dbConnection;
+        private string databaseName;
+
         public ClientSQL(string connectionString, string databaseName)
         {
+            // connectionString = @"Server=serverName;Database=databaseName;User ID=sa;Password=password;"
             this.connectionString = connectionString;
-            this.dbConnection = new SqlConnection(connectionString);
+            this.databaseName = databaseName;
+            try
+            {
+                this.server = new Server(new ServerConnection(new SqlConnection(connectionString)));
+            } 
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
         }
 
         public async Task<dbResponse> CreateNewDatabase(string databaseName)
+        {
+            var db = new Database(this.server, databaseName);
+            try
+            {
+                db.Create();
+                MessageBox.Show($"DataBase '{databaseName}' is created successfully", "Sucess", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return new dbResponse(true, $"DataBase '{databaseName}' created successfully.");
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return new dbResponse(false, $"Error: {ex.Message}");
+            }
+        }
+
+        public async Task<dbResponse> CreateNewDatabaseByCommand(string databaseName)
         {
             var conn = this.dbConnection;
             var str = $"CREATE DATABASE '{databaseName}'";
@@ -38,9 +64,26 @@ namespace SEP.ClientDatabase
                 return new dbResponse(false, $"Error: {ex.Message}");
             }
         }
-
         public async Task<dbResponse> DeleteDatabaseByName(string databaseName)
         {
+            var db = new Database(this.server, databaseName);
+            try
+            {
+                db.Drop();
+                MessageBox.Show($"DataBase '{databaseName}' is deleted successfully", "Sucess", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return new dbResponse(true, $"DataBase '{databaseName}' deleted successfully.");
+            }
+            catch (System.Exception ex)
+            {
+                db.Drop();
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return new dbResponse(false, $"Error: {ex.Message}");
+            }
+        }
+
+        public async Task<dbResponse> DeleteDatabaseByNameByCommand(string databaseName)
+        {
+
             var str = $"DROP DATABASE '{databaseName}'";
             var conn = this.dbConnection;
             SqlCommand command = new SqlCommand(str, conn);
@@ -60,5 +103,6 @@ namespace SEP.ClientDatabase
                 return new dbResponse(false, $"Error: {ex.Message}");
             }
         }
+
     }
 }
