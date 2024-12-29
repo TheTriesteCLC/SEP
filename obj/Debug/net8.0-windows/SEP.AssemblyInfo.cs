d81aa9661fd978b933bd02077015e615522711cf
npt306 <nptai212@clc.fitus.edu.vn>
2024-12-29 19:20:46 +0700

    view method for sql server

--- obj/Debug/net8.0-windows/SEP.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SEP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SEP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8432931838d8c8b81611d2d69d7fac95878f4a28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e76a022df087803686318959422a6cc62e2d8af")]
 [assembly: System.Reflection.AssemblyProductAttribute("SEP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SEP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
